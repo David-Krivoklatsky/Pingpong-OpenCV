3ca3c25f3b224a9dc966fbb9b2b612dede5ac6ad
David Krivoklatsky <dado70dado70@gmail.com>
2023-01-28 17:54:27 +0100

    filip mi to rozbehol

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
 int main() {
 
 	std::cout << "Hello world!";
+
+	cv::Mat image = cv::imread("C:\\Users\\Krivoklatsky\\Pictures\\Einladung.png");
+	cv::resize(image, image, image.size() / 2);
+	cv::imshow("test window", image);
+
+	cv::waitKey(0);
+
 	return 0;
 }
